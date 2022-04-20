e406ef6139378fbbba1e718d134cda01e4ff8f44
fToxicw <ftoxicw@163.com>
2022-04-20 21:15:42 +0800

    Update tcp_client.py

--- Basic-Network/tcp_client.py
+++ Basic-Network/tcp_client.py
@@ -1,13 +1,21 @@
+'''
+This is a simple TCP client made from Python socket package. Change the parameters to your own before running the program. They are already highlighted in the file.
+'''
+# Import needed packages
 import socket
 
-HOST = 'www.google.com'
-PORT = 80
+# Host and port
+HOST = '<Your host here>'
+PORT = <Your port here>
 
+# Create a socket object and connect to the server using HOST and PORT
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-client.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
+# Send data and receive data
+client.send(b'GET / HTTP/1.1\r\nHost: <Your host here>\r\n\r\n')
 response = client.recv(4096)
 print(response.decode('utf-8'))
 
+# Close the connection
 client.close()
