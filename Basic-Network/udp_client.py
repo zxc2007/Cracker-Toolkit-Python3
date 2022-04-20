c995b5343bce9c7ad676a34eb24c0ed4e2676332
fToxicw <ftoxicw@163.com>
2022-04-20 21:20:01 +0800

    Update udp_client.py

--- Basic-Network/udp_client.py
+++ Basic-Network/udp_client.py
@@ -1,13 +1,21 @@
+'''
+A simple UDP client written in Python with the socket package. Change the parameters that are highlighted in the file before running it.
+'''
+# Import needed packages
 import socket
 
-HOST = '127.0.0.1'
-PORT = 9997
+# Host and port
+HOST = '<Your host here>'
+PORT = <Your port here>
 
+# Create a socket object and send some data to the host
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.sendto(b'AAABBBCCC', (HOST, PORT))
 
+# Receive data from the host
 data, address = client.recvfrom(4096)
 print(data.decode('utf-8'))
 print(address.decode('utf-8'))
 
+# Close the connection
 client.close()
