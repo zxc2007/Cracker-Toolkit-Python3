08e69f5bb502d749a8d88eef4d105c71814f561b
fToxicw <ftoxicw@163.com>
2022-04-23 11:04:35 +0800

    Update ssh_cmd.py

--- Basic-Network/ssh_cmd.py
+++ Basic-Network/ssh_cmd.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
+
+'''
+A SSH tool built with Python.
+
+The Netcat tool we built is pretty handy, but sometimes it's wise to encrypt your traffic to avoid detection. A common means of doing so is to tunnel the traffic using Secure Shell (SSH). But what if your target doesn't have an SSH client, just like 99.81943 percent of Windows systems? That is why we will need to build a SSH tool using Python.
+'''
+# Import needed packages
 import paramiko
 
+# Create a SSH Client, connect to the host, and send the command.
 def ssh_command(ip, port, user, passwd, cmd):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -13,13 +21,14 @@ def ssh_command(ip, port, user, passwd, cmd):
         for line in output:
             print(line.strip())
 
+# Run the program
 if __name__ == '__main__':
-    import getpass
+    import getpass # Needed for hiding password
     # user = getpass.getuser()
     user = input('Username: ')
-    password = getpass.getpass()
+    password = getpass.getpass() # Hide password during the input
 
-    ip = input('Enter server IP: ') or '192.168.1.203'
+    ip = input('Enter server IP: ') or '127.0.0.1'
     port = input('Enter port or <CR>: ') or 2222
     cmd = input('Enter command or <CR>: ') or 'id'
     ssh_command(ip, port, user, password, cmd)
